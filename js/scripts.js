13aeb0404c88b23bfd291ff89f6462b6cbb681cc
Hermann <91563884+HerRA17@users.noreply.github.com>
2022-08-02 12:02:33 +0200

    Created an empty array and four objects
    An array was created empty and four objects were pushed to the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
-alert('Hello World!');
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+let pokemonList = [];
+
+const pokemonOne = {
+    name:'Charizard',
+    height: '1.7m',
+    type: 'fire, flying'
+};
+const pokemonTwo = {
+    name:'Pikachu',
+    height:'.4m',
+    type: 'electric'
+}; 
+const pokemonThree ={
+    name:'Mewtwo',
+    height: '2m',
+    type: 'psychic'
+};
+const pokemonFour = {
+    name: 'Articuno',
+    height:'1.7m',
+    type:'flying, ice'
+};
+
+pokemonList.push(pokemonOne);
+pokemonList.push(pokemonTwo);
+pokemonList.push(pokemonThree);
+pokemonList.push(pokemonFour);
+console.log(pokemonList.length);
+console.log(pokemonList);
+
