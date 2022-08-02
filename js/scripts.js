ba33297c64ae58864eee4a177c8883e36089508e
Hermann <91563884+HerRA17@users.noreply.github.com>
2022-08-02 20:03:13 +0200

    Update to the array display
    A for loop was added to the array (revision of .height value).
    The big comment is a thought, meaning if I could display the value of the array inside of an <div>, additionally to another layout.

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,26 @@ pokemonList.push(pokemonOne);
 pokemonList.push(pokemonTwo);
 pokemonList.push(pokemonThree);
 pokemonList.push(pokemonFour);
-console.log(pokemonList.length);
-console.log(pokemonList);
 
+/*
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].type  === 'fire, flying' ) {
+        document.write(pokemonList[i].name + ' is an incredible ' + (pokemonList[i].height) +' beast!');
+    } else if (pokemonList[i].type  === 'psychic' || pokemonList[i].height === '2m') {
+        document.write(pokemonList[i].name + ' is so to speak a clone of mew and is ' + (pokemonList[i].height) + ' tall and extremly rare!');
+    } else if (pokemonList[i].type  === 'flying, ice') {
+        document.write(pokemonList[i].name + ' is a legendary ' + pokemonList[i].type + ' rare to encounter!');
+    };
+    document.write(pokemonList);
+};
+I would like to know if I could write it like that including ('div class="pokemonList"') insted of pokemonList and make it appear inside.
+For now I have not been succesful.
+*/
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > '1.0' ) {
+        document.write(pokemonList[i].name + ' (' + pokemonList[i].height + ')' + '-it is a big pokemon!');
+    } else {
+        document.write(pokemonList[i].name +' (' + pokemonList[i].height + ')' + '-not big but could surprise you.');
+    }
+}; //for loop rivision of height-value.
