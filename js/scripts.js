b2318a056b7fad86295a99ac445742e6e4242951
Hermann <91563884+HerRA17@users.noreply.github.com>
2022-08-09 21:01:56 +0200

    Update to the loop
    Provides a better and simple display method., including HTML elements insertion.

--- js/scripts.js
+++ js/scripts.js
@@ -26,25 +26,10 @@ pokemonList.push(pokemonTwo);
 pokemonList.push(pokemonThree);
 pokemonList.push(pokemonFour);
 
-/*
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].type  === 'fire, flying' ) {
-        document.write(pokemonList[i].name + ' is an incredible ' + (pokemonList[i].height) +' beast!');
-    } else if (pokemonList[i].type  === 'psychic' || pokemonList[i].height === '2m') {
-        document.write(pokemonList[i].name + ' is so to speak a clone of mew and is ' + (pokemonList[i].height) + ' tall and extremly rare!');
-    } else if (pokemonList[i].type  === 'flying, ice') {
-        document.write(pokemonList[i].name + ' is a legendary ' + pokemonList[i].type + ' rare to encounter!');
-    };
-    document.write(pokemonList);
-};
-I would like to know if I could write it like that including ('div class="pokemonList"') insted of pokemonList and make it appear inside.
-For now I have not been succesful.
-*/
-
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > '1.0' ) {
-        document.write(pokemonList[i].name + ' (' + pokemonList[i].height + ')' + '-it is a big pokemon!');
+for (const {name, height} of pokemonList) {
+    if (height > 1.0 ) {
+        document.write(`<p class="large-pokemon">${name}, is (${height})m, and an ${pokemon.type}. This is a big Pokémon!</p>`);
     } else {
-        document.write(pokemonList[i].name +' (' + pokemonList[i].height + ')' + '-not big but could surprise you.');
+        document.write(`<p class="small-pokemon">${name}, is (${height})m, and an ${pokemon.type}. Might not be big but could surprise you.</p>`);
     }
-}; //for loop rivision of height-value.
+}
