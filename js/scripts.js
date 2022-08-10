1d2909e15709c8233e77ef386d88799dd9c1dfa8
Hermann <91563884+HerRA17@users.noreply.github.com>
2022-08-10 12:52:26 +0200

    Change the structure of the script.js
    Function with local scope and constant variable defined within the function.
    Although it is not responding as expected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,44 @@
-let pokemonList = [];
+    
+const pokemonRepository = (function (){
+     const pokemonList = [];
+ const pokemonOne = {
+     name:'Charizard',
+     height: 1.7,
+     type: ['fire', 'flying']
+ };
+ const pokemonTwo = {
+     name:'Pikachu',
+     height: 0.4,
+     type: 'electric'
+ }; 
+ const pokemonThree ={
+     name:'Mewtwo',
+     height: 2.0,
+     type: 'psychic'
+ };
+ const pokemonFour = {
+     name: 'Articuno',
+     height:1.7,
+     type:['flying' , 'ice']
+ }; //object
+     function add(pokemon) {
+         if (typeof pokemon === 'object' && 'name' in pokemon) {
+             pokemonList.push(pokemon); } else {
+                 console.log('pokemon is not correct')
+                 }
+             }
+     function getAll() {
+             return pokemonList.getAll()
+             }
+        
+     function remove() {
+             pokemonList.pop(pokemon);
+             }           
+         return{ 
+             add: add,
+             getAll: getAll,
+             remove: remove
+           }
 
-const pokemonOne = {
-    name:'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying']
-};
-const pokemonTwo = {
-    name:'Pikachu',
-    height:0.4,
-    type: 'electric'
-}; 
-const pokemonThree ={
-    name:'Mewtwo',
-    height: 2.0,
-    type: 'psychic'
-};
-const pokemonFour = {
-    name: 'Articuno',
-    height:1.7,
-    type:['flying', 'ice']
-};
 
-pokemonList.push(pokemonOne);
-pokemonList.push(pokemonTwo);
-pokemonList.push(pokemonThree);
-pokemonList.push(pokemonFour);
-
-for (const {name, height} of pokemonList) {
-    if (height > 1.0 ) {
-        document.write(`<p class="large-pokemon">${name}, is (${height})m, and an ${pokemon.type}. This is a big Pokémon!</p>`);
-    } else {
-        document.write(`<p class="small-pokemon">${name}, is (${height})m, and an ${pokemon.type}. Might not be big but could surprise you.</p>`);
-    }
-}
+ }) (); //IIFE
