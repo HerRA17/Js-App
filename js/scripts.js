d3470a32f3fb56e4619eb692fbb4e8030c717322
Hermann <91563884+HerRA17@users.noreply.github.com>
2022-08-11 21:48:50 +0200

    Update to the loops and IIFE
    I found a better solution to my existing code.
    Function in local scope avoiding being accessed by accident.
    Looping allows the classification of pokemons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-const pokemonOne = {
+
+//local scope array and functions
+const pokemonRepository = (function (){
+    //objects
+    const pokemonOne = {
     name:'Charizard',
     height: 1.7,
     type: ['fire', 'flying']
@@ -18,12 +22,13 @@ const pokemonFour = {
     height:1.7,
     type:['flying', 'ice']
 };
-//local scope array and functions
-const pokemonRepository = (function (){
     const pokemonList = [];
 //addition of pokemon
     function add(pokemon) {
-        if (typeof pokemon === 'object' && 'name' in pokemon) {
+        if (typeof pokemon === 'object' &&
+        'name' in pokemon &&
+        'height' in pokemon &&
+        'types' in pokemon) {
             pokemonList.push(pokemon); } else {
                 console.log('pokemon is not correct');
                 // document.alert('pokemon is not correct');
@@ -33,41 +38,32 @@ const pokemonRepository = (function (){
     function getAll() {
             return pokemonList;
         }
-        //remove pokemons; even though is not properly working
-    function remove() {
-            pokemonList.pop(pokemon);
-        }           
-        return{ 
+    return{ 
             add: add,
             getAll: getAll,
-            remove: remove
-          }
+           }
 
 }) (); //IIFE
-pokemonRepository.add(pokemonOne);
-pokemonRepository.add(pokemonTwo);
-pokemonRepository.add(pokemonThree);
-pokemonRepository.add(pokemonFour);
 // simplyfying loop-forEach
-function clasification(pokemon) {
-        // let str = `<p>${pokemon.name}, is (${pokemon.height})m, and an ${pokemon.type}.</p>`;
-    if (pokemon.type[0] === 'fire' && pokemon.type[1] === 'flying') {
-        document.write(`<p id="fire-flying-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`); 
-    }  else if (pokemon.type === 'electric' ) {
-        document.write(`<p id="electric-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // document.write(str); 
-    } else if (pokemon.type === 'psychic' ) {
-        document.write(`<p id="psychic-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // document.write(str); //I am not sure if that should as well be part of it?
-    } else if (pokemon.type[0] === 'ice' && pokemon.type[1] === 'flying' ) {
-        document.write(`<p id="ice-flying-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // document.write(str); 
-    }
-    
+pokemonRepository.getAll().forEach(clasification(pokemon)) {
+    let str = `<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`;
+if (pokemon.type[0] === 'fire' && pokemon.type[1] === 'flying') {
+    document.write(str);
+    // document.write(`<p id="fire-flying-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
+    console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`); 
+}  else if (pokemon.type === 'electric' ) {
+    // document.write(str);
+    // document.write(`<p id="electric-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
+    console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
+} else if (pokemon.type === 'psychic' ) {
+    // document.write(str);
+    // document.write(`<p id="psychic-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
+    console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
+} else if (pokemon.type[0] === 'ice' && pokemon.type[1] === 'flying' ) {
+    // document.write(str);
+    // document.write(`<p id="ice-flying-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
+    console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
+}
 }
 // console.log(pokemonList.forEach(clasification));
 pokemonList.forEach(clasification);
