0f309f1a6cd5a248f16c653138761ca66ba73d87
Hermann <91563884+HerRA17@users.noreply.github.com>
2022-08-12 17:25:13 +0200

    Update on IIFE and loop function
    Addition of local scope of the pokemon list with objects.
    Addition of function add( adds a new pokemon to the pokemon list). 
    Addition of function  getAll( retrieves all pokemons).
    Classifies pokemons depending on their type(through the classification function).

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,58 @@
-let pokemonList = [];
-
-const pokemonOne = {
-    name:'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying']
-};
-const pokemonTwo = {
-    name:'Pikachu',
-    height:0.4,
-    type: 'electric'
-}; 
-const pokemonThree ={
-    name:'Mewtwo',
-    height: 2.0,
-    type: 'psychic'
-};
-const pokemonFour = {
-    name: 'Articuno',
-    height:1.7,
-    type:['flying', 'ice']
-};
-
-pokemonList.push(pokemonOne);
-pokemonList.push(pokemonTwo);
-pokemonList.push(pokemonThree);
-pokemonList.push(pokemonFour);
-
-// simplyfying loop-forEach
-function clasification(pokemon) {
-        // let str = `<p>${pokemon.name}, is (${pokemon.height})m, and an ${pokemon.type}.</p>`;
-    if (pokemon.type[0] === 'fire' && pokemon.type[1] === 'flying') {
-        document.write(`<p id="fire-flying-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`); 
-    }  else if (pokemon.type === 'electric' ) {
-        document.write(`<p id="electric-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // document.write(str); 
-    } else if (pokemon.type === 'psychic' ) {
-        document.write(`<p id="psychic-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // document.write(str); //I am not sure if that should as well be part of it?
-    } else if (pokemon.type[0] === 'ice' && pokemon.type[1] === 'flying' ) {
-        document.write(`<p id="ice-flying-pokemon">${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // console.log(`<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type}.</p>`);
-        // document.write(str); 
+const pokemonRepository = (function (){
+//objects
+    const pokemonList = [
+    {
+        name:'Charizard',
+        height: 1.7,
+        type: ['fire' , 'flying']
+    },
+    {
+        name:'Pikachu',
+        height: 0.4,
+        type: 'electric'
+    }, 
+    {
+        name:'Mewtwo',
+        height: 2.0,
+        type: 'psychic'
     }
-    
+    ];
+
+    function add(pokemon) {
+        if (typeof pokemon === 'object' && 
+        'name' in pokemon &&
+        'height' in pokemon &&
+        'type' in pokemon) {
+            pokemonList.push(pokemon); 
+        } else {
+                console.log('pokemon is not correct');
+                // document.alert('pokemon is not correct');
+               }
+            }
+    function getAll() {
+            return pokemonList;
+        }
+        
+        return{ 
+            add: add,
+            getAll: getAll,
+        }
+        
+}) (); //IIFE
+
+pokemonRepository.add({name: 'Articuno',     height:1.7,    type:[ 'ice', 'flying']}); 
+// simplyfying loop-forEach pokemon
+ function clasification(pokemon) {
+    let str = `<p>${pokemon.name}, is ${pokemon.height}m, and an ${pokemon.type} pokemon.</p>`;
+if (pokemon.type[0] === 'fire' && pokemon.type[1] === 'flying') {
+    document.write(str);
+}  else if (pokemon.type === 'electric' ) {
+    document.write(str);
+} else if (pokemon.type === 'psychic' ) {
+    document.write(str);
+} else if (pokemon.type[0] === 'ice' && pokemon.type[1] === 'flying' ) {
+    document.write(str);
+    }
 }
-// console.log(pokemonList.forEach(clasification));
-pokemonList.forEach(clasification);
+
+pokemonRepository.getAll().forEach(clasification); 
