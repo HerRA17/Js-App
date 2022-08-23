424119afd8e56c8bd49ea09bda3f6b70c575131c
Hermann <91563884+HerRA17@users.noreply.github.com>
2022-08-23 17:05:53 +0200

    Addition of function to mimic pop up/modal
    Addition of a show & hide modal.
    Addition of show dialog function.
    Addition of an event listener display an alert if confirmed changes or not.
    Addition of an event listener when used the key escape the pop up should be able to close.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ const pokemonRepository = (function (){
     const pokemonList = [];
     //api-url of pokemons
     let apiUrl ='https://pokeapi.co/api/v2/pokemon/?limit=150';
+    //container of modal function
+    // let modalContainer = document.querySelector('#modal-container');
+    // not sure how to call it
+    let dialogPromiseReject;
     //function add pokemon
     function add(pokemon) {
         if (typeof pokemon === 'object' && 
@@ -37,6 +41,7 @@ const pokemonRepository = (function (){
             showDetails(pokemon);
             });
         }
+
     //load list function
     function loadList() {
         return fetch(apiUrl).then(function (response) {
@@ -52,7 +57,7 @@ const pokemonRepository = (function (){
                 });
             }).catch(function (e) {
                 console.error(e);
-            }) 
+        }) 
         };
     //load details function
     function loadDetails(item) { 
@@ -69,10 +74,102 @@ const pokemonRepository = (function (){
     }
      //shows details of pokemon
     function showDetails(pokemon){
-        pokemonRepository.loadDetails(pokemon).then(function(){
-            console.log(pokemon);
-        });
+    //addition of modal functionality
+        pokemonRepository.loadDetails(pokemon).then(function
+            (){
+                showModal(pokemon);
+                console.log(pokemon);
+            });
     }
+        
+        //Show Modal function
+        function showModal(title, text) {
+            modalContainer.innerHTML = '';
+            // let modalContainer = document.createElement('div');
+            // modalContainer.setAttribute('id', 'modal-container');
+            let modalContainer = document.querySelector('#modal-container');
+            //create modal element & giving it a class
+            let modal = document.createElement('div');
+            modal.classList.add('modal');
+            //add new modal
+            let closeButtonElement = document.createElement('button');
+            closeButtonElement.classList.add('modal-close');
+            closeButtonElement.innerText = 'Close';
+            closeButtonElement.addEventLister('click' , hideModal);
+            //add title
+            let titleElement = document.createElement('h1');
+            titleElement.innerText = pokemon.name.chartArt(0).toUpperCase() + pokemon.name.slice(1);
+            //add content
+            let contentElement = document.createElement('p');
+            contentElement.innerText = `Height: ${pokemon.height}`;
+            //add image
+            let imgElement = document.createElement('img');
+            imgElement.classList.add('pokemon-img')
+            imgElement.src = pokemon.imgUrl; 
+            //appending Child elements of modal and modal Container
+            //append container modal-check if 
+            modalContainer.append;
+            modal.appendChild(closeButtonElement);
+            modal.appendChild(titleElement);
+            modal.appendChild(contentElement);
+            modal.appendChild(imgElement);
+            modalContainer.appendChild(modal);
+            modalContainer.classList.add('is-visible');
+        }
+        //Hide Modal function
+        function hideModal() {
+            modalContainer.classList.remove('is-visible');
+            //condition statement for the removal of class
+            if(dialogPromiseReject) {
+                dialogPromiseReject();
+                dialogPromiseReject = null;
+            }
+        }
+        function showDialog(title,text) {
+            showModal(title,text);
+            //select modal-class
+            let modal = modalContainer.querySelector('.modal');
+            //create confirm button, class addition and setting inner Text
+            let confirmButton = document.createElement('button');
+            confirmButton.classList.add('modal-confirm');
+            confirmButton.innerText = 'Confirm';
+            //create  cancel button, class addition and setting inner Text
+            let cancelButton = document.createEleemnte('button');
+            cancelButton.classList.add('modal-cancel')
+            cancelButton.innerText = 'Cancel';
+            //append childs to modal
+            modal.appendChild(confirmButton);
+            modal.appendChild(cancelButton);
+            //set Focus to confirm to easily select button with keyboard
+            confirmButton.focus();
+            //Promise
+            return new Promise((resolve, reject) => {
+                cancelButton.addEventListener('click', hideModal);
+                confirmButton.addEventListener('click', () =>{
+                    dialogPromiseReject = null;
+                    hideModal();
+                    resolve();
+                });
+                dialogPromiseReject = reject;
+            });
+        }
+        //Even listener for text display either confirm or not 
+        document.querySelector('#show-modal').addEventListener('click', () => {
+            showDialog('Confirm action', 'Are you sure you want to do this?').then(function() {
+                alert('confirmed!');
+            }, () => {
+                alert('not confirmed!')
+            });
+        });
+        //event listener to exit with key-escape
+        window.addEventListener('keydown', (e) => {
+            let modalContainer = document.querySelector('#modal-container');
+            if(e.key === 'Escape' &&
+            modalContainer.classList.contains('is-visible')) {
+                hideModal();
+            }
+        });
+    
         return{ 
             add: add,
             getAll: getAll,
@@ -82,13 +179,10 @@ const pokemonRepository = (function (){
             showDetails:showDetails
         };
 
-}) ();
+})();
 
 pokemonRepository.loadList().then(function(){
     pokemonRepository.getAll().forEach(function(pokemon) {
         pokemonRepository.addListItem(pokemon);
     });
 });
-
-
-
